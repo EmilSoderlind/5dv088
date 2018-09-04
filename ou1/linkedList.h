2780024d05a0287b9659d00ee00782af880db14a
Emil Söderlind <id15esd@cs.umu.se>
2018-09-04 11:55:34 +0200

    Added mpasswdsort.c

--- ou1/linkedList.h
+++ ou1/linkedList.h
@@ -1,1 +1,12 @@
 int returnNumber(int);
+
+typedef struct node {
+  struct node* prev;
+  void* value;
+  struct node* next;
+}
+
+typedef struct user_info {
+  unsigned int uid;
+  char* uname;
+};
