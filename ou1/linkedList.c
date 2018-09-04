67b2d8b52dac7d7df56f638dbc6d0d87208ffb79
Emil Söderlind <id15esd@cs.umu.se>
2018-09-04 11:06:41 +0200

    Added hello-world

---
+++ ou1/linkedList.c
@@ -0,0 +1,10 @@
+#include "linkedList.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   // printf() displays the string inside quotation
+   printf("Hello, World!\n");
+   return 0;
+}
