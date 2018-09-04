946c889a42690da7b113cf94b035069ca75c0ac4
Emil Söderlind <id15esd@cs.umu.se>
2018-09-04 11:20:53 +0200

    Added header stuff

--- ou1/linkedList.c
+++ ou1/linkedList.c
@@ -3,8 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(){
    // printf() displays the string inside quotation
-   printf("Hello, World!\n");
+   int h = returnNumber(1337);
+   printf("Hello, World! %d \n",h);
    return 0;
+}
+
+int returnNumber(int k){
+  return k;
 }
