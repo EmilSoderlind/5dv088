078fee8af365d1c76bc0df5106ec4dc00539a4b5
Emil Söderlind <id15esd@cs.umu.se>
2018-09-05 11:18:51 +0200

    Inputs stdin OR file

--- ou1/mpasswdsort.c
+++ ou1/mpasswdsort.c
@@ -4,100 +4,77 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <string.h>
 
-void fileDemo(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+	printf("Starting mpasswdsort.c\n");
 
-  bool parsedInput = false;
+	printf("argc: %d\n", argc);
 
-  while(!parsedInput){
+	FILE *fp;
 
-    FILE *fp;
-    char ch;
+	// OM vi har input-parameters ta hand om dom
+	if (argc == 2)
+	{
+		// Om filen fuckar --> felmeddelande
+		fp = fopen(argv[1], "r");
+	
+	}
+	else if (argc == 1)
+	{ // OM inga input-parameters, kolla stdin.
 
-        fp=fopen(argv[1],"r");
+		fp = stdin;
 
-        if(fp==NULL){
-            printf("Some problem in opening the file\n");
+		// Om ingen stdin --> felmeddelande
+	}
+	else if (argc > 2)
+	{ // OM vi har för många input-parameters --> felmeddelande
+		fprintf(stderr, "ERROR: To many parameters\n");
+		exit(EXIT_FAILURE);
+	}
 
-            // ERROR MESSAGE
-            exit(0);
-        }else{
-            while((ch=fgetc(fp))!=EOF){
-                printf("%c",ch);
-            }
-            break; // TO BE REMOVED
-        }
+	if (fp == NULL)
+	{
+		fprintf(stderr, "ERROR: Could not open file\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		char ch;
+		while ((ch = fgetc(fp)) != EOF)
+		{
 
-        fclose(fp);
+			if (strcmp(&ch, "\n") == 0)
+			{
+				printf("---End of line!\n");
+			}
+			else
+			{
+				printf("%c", ch);
+			}
+		}
+	}
 
-      }
+	fclose(fp);
 
-}
+	node *rootNode = linkedList_create();
 
-int main(int argc, char *argv[]){
-  printf("Starting mpasswdsort.c\n");
+	user_info *value = malloc(sizeof(user_info));
+	value->uid = 3;
+	value->uname = "Emil";
 
-  printf("argc: %d\n", argc);
+	user_info *valueTwo = malloc(sizeof(user_info));
+	valueTwo->uid = 24;
+	valueTwo->uname = "Simon";
 
-
-// OM vi har input-parameters ta hand om dom
-  if(argc == 2){
-
-    // Om filen fuckar --> felmeddelande
-
-
-  }else if(argc == 1){ // OM inga input-parameters, kolla stdin.
-
-    // Om ingen stdin --> felmeddelande
-
-
-  }else if(argc > 2){ // OM vi har för många input-parameters --> felmeddelande
-    printf("ERROR: To many parameters\n"); //TODO Fixa stderr senare
-
-  }
-
-
-
-fileDemo(argc, argv);
-
-  node *rootNode = linkedList_create();
-
-
-    user_info *value = malloc(sizeof(user_info));
-    value->uid = 3;
-    value->uname = "Emil";
-
-    user_info *valueTwo = malloc(sizeof(user_info));
-    valueTwo->uid = 24;
-    valueTwo->uname = "Simon";
-
-
-  linkedList_append(rootNode, value);
-  linkedList_append(rootNode, valueTwo);
+	linkedList_append(rootNode, value);
+	linkedList_append(rootNode, valueTwo);
 
 
-/*
-  char *text = "Hello";
-  unsigned int i = 5;
-  user_info* valueThree = user_info_create(i,text);
+	linkedList_print(rootNode);
 
-  linkedList_append(rootNode, valueThree);
-*/
+	linkedList_free(rootNode);
 
-  linkedList_print(rootNode);
-
-  linkedList_free(rootNode);
-
-  return 0;
-}
-
-
-
-
-user_info* user_info_create(int i, char *name){
-  user_info *ui = malloc(sizeof(user_info));
-  ui->uid = (unsigned int) i;
-  ui->uname = name;
-
-return ui;
+	return 0;
 }
