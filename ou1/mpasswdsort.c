8f60a76a023249c08d8d66aeee48771a9e3e124a
Emil Söderlind <id15esd@cs.umu.se>
2018-09-05 17:54:32 +0200

    workiiing

--- ou1/mpasswdsort.c
+++ ou1/mpasswdsort.c
@@ -12,14 +12,15 @@ int main(int argc, char *argv[])
 
 	printf("argc: %d\n", argc);
 
-	FILE *fp;
+	node *unsortedList = linkedList_create();
 
+	FILE *fp;
 	// OM vi har input-parameters ta hand om dom
 	if (argc == 2)
 	{
 		// Om filen fuckar --> felmeddelande
 		fp = fopen(argv[1], "r");
-	
+
 	}
 	else if (argc == 1)
 	{ // OM inga input-parameters, kolla stdin.
@@ -27,6 +28,11 @@ int main(int argc, char *argv[])
 		fp = stdin;
 
 		// Om ingen stdin --> felmeddelande
+		if (0) //TODO FIX
+		{
+			fprintf(stderr, "ERROR: No input\n");
+			exit(EXIT_FAILURE);
+		}
 	}
 	else if (argc > 2)
 	{ // OM vi har för många input-parameters --> felmeddelande
@@ -41,23 +47,98 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		char ch;
-		while ((ch = fgetc(fp)) != EOF)
-		{
+		int lineNumber = 0;
+		char *line;
+		size_t bufsize = 1023;
+		int characters;
+		bool reachedEOF = false;
 
-			if (strcmp(&ch, "\n") == 0)
-			{
-				printf("---End of line!\n");
+		printf("JAKUUB: %s<---\n",line);
+
+		line = malloc(bufsize * sizeof(char));
+		if (line == NULL)
+		{
+			fprintf(stderr, "ERROR: Unable to allocate buffer\n");
+			exit(1);
+		}
+
+
+		while (!reachedEOF)
+		{
+			if (getline(&line, &bufsize, fp) != -1)
+			{ // EOF
+				line[strlen(line) - 1] = 0; // Remove last /n-char
+
+				for(int i = 0; i<strlen(line);i++){ // Replace " " with _
+					if(line[i] == ' '){
+						line[i] = '_';
+					}
+				}
+
+				for (int i = 0; i < strlen(line); i++) // Replace ":" with " "
+				{
+					if (line[i] == ':')
+					{
+						line[i] = ' ';
+					}
+				}
+
+				char *username = malloc(bufsize * sizeof(char));
+				char *password = malloc(bufsize * sizeof(char));
+				char *UID = malloc(bufsize * sizeof(char));
+				char *GID = malloc(bufsize * sizeof(char));
+				char *GECOS = malloc(bufsize * sizeof(char));
+				char *directory = malloc(bufsize * sizeof(char));
+				char *shell = malloc(bufsize * sizeof(char));
+
+
+				sscanf(line, "%s %s %s %s %s %s %s", username, password, UID, GID, GECOS, directory, shell);
+
+				// CHECK STUFF
+
+
+
+
+
+				// ADD TO LIST
+				user_info *newValue = malloc(sizeof(user_info));
+				newValue->uid = strtol(UID);
+				newValue->uname = "Simon";
+
+
+
+				printf("username: %s ", username);
+				printf("password: %s ", password);
+				printf("UID: %s ", UID);
+				printf("GID: %s ", GID);
+				printf("GECOS: %s ", GECOS);
+				printf("directory: %s ", directory);
+				printf("shell: %s\n", shell);
+
+
+				free(username);
+				free(password);
+				free(UID);
+				free(GID);
+				free(GECOS);
+				free(directory);
+				free(shell);
 			}
 			else
 			{
-				printf("%c", ch);
+				reachedEOF = true;
 			}
+			lineNumber++;
 		}
-	}
+
+		free(line);
 
 	fclose(fp);
 
+	linkedList_print(unsortedList);
+	linkedList_free(unsortedList);
+
+	/*
 	node *rootNode = linkedList_create();
 
 	user_info *value = malloc(sizeof(user_info));
@@ -72,9 +153,7 @@ int main(int argc, char *argv[])
 	linkedList_append(rootNode, valueTwo);
 
 
-	linkedList_print(rootNode);
-
-	linkedList_free(rootNode);
+	linkedList_free(rootNode);*/
 
 	return 0;
 }
