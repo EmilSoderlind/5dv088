16e257757749046dd30653375aca01abb5eb7469
Emil Söderlind <id15esd@cs.umu.se>
2018-09-06 16:35:30 +0200

    Reworked split

--- ou1/mpasswdsort.c
+++ ou1/mpasswdsort.c
@@ -6,27 +6,25 @@
 #include <errno.h>
 #include <string.h>
 
+
 int main(int argc, char *argv[])
 {
 
 	node *list = linkedList_create();
-
 	FILE *fp;
 
 	bool foundFaultyRow = false;
 
-	if (argc == 2) // OM vi har input-parameters ta hand om dom
+	if (argc == 2) // If we got input-parameters
 	{
-		// Om filen fuckar --> felmeddelande
 		fp = fopen(argv[1], "r");
-	
 	}
 	else if (argc == 1) // Check STDIN
 	{ 
 		fp = stdin;
 	}
-	else if (argc > 2)
-	{ // OM vi har för många input-parameters --> felmeddelande
+	else if (argc > 2) // Too many input-parameters
+	{ 
 		fprintf(stderr, "ERROR: To many parameters\n");
 		exit(EXIT_FAILURE);
 	}
@@ -56,32 +54,93 @@ int main(int argc, char *argv[])
 			{
 				bool faulthy_row = false;
 
-				line[strlen(line) - 1] = 0; // Remove last /n-char
-
-				for(int i = 0; i<strlen(line);i++){ // Replace " " with _
-					if(line[i] == ' '){
-						line[i] = '_';
-					}
-				}
-
+				int countingSemiColons = 0;
 				for (int i = 0; i < strlen(line); i++) // Replace ":" with " "
 				{
 					if (line[i] == ':')
 					{
-						line[i] = ' ';
+						countingSemiColons++;
 					}
 				}
 
-				char *UID = malloc(bufsize * sizeof(char));
-				char *username = malloc((bufsize+1) * sizeof(char));
+				line[strlen(line) - 1] = 0; // Remove last /n-char
+		
+				char *UID = calloc(bufsize, sizeof(char));
+				char *username = calloc((bufsize+1),sizeof(char));
 
-				char *password = malloc(bufsize * sizeof(char));
-				char *GID = malloc(bufsize * sizeof(char));
-				char *GECOS = malloc(bufsize * sizeof(char));
-				char *directory = malloc(bufsize * sizeof(char));
-				char *shell = malloc(bufsize * sizeof(char));
+				char *password = calloc(bufsize, sizeof(char));
+				char *GID = calloc(bufsize, sizeof(char));
+				char *GECOS = calloc(bufsize,sizeof(char));
+				char *directory = calloc(bufsize, sizeof(char));
+				char *shell = calloc(bufsize, sizeof(char));
 
-				sscanf(line, "%s %s %s %s %s %s %s", username, password, UID, GID, GECOS, directory, shell);
+				//sscanf(line, "%s %s %s %s %s %s %s", username, password, UID, GID, GECOS, directory, shell);
+				
+				int workingOnFieldNr = 0;
+
+				int usernameLength = 0;
+				int passwordLength = 0;
+				int UIDLength = 0;
+				int GIDLength = 0;
+				int GECOSLength = 0;
+				int directoryLength = 0;
+				int shellLength = 0;
+
+				for(int j = 0; j<strlen(line); j++){
+
+					if(line[j] == ':'){
+						workingOnFieldNr++;
+					}else{
+
+						switch (workingOnFieldNr)
+						{
+						case 0:
+							username[usernameLength] = line[j];
+							usernameLength++;
+							break; 
+						case 1:
+							password[passwordLength] = line[j];
+							passwordLength++;
+							break;
+						case 2:
+							UID[UIDLength] = line[j];
+							UIDLength++;
+							break;
+						case 3:
+							GID[GIDLength] = line[j];
+							GIDLength++;
+							break;
+						case 4:
+							GECOS[GECOSLength] = line[j];
+							GECOSLength++;
+							break;
+						case 5:
+							directory[directoryLength] = line[j];
+							directoryLength++;
+							break;
+						case 6:
+							shell[shellLength] = line[j];
+							shellLength++;
+							break;
+						default:
+							printf("Line %d: Encountered too many semicolons. Invalid format.\n", lineNumber);
+							faulthy_row = true;
+							free(username);
+						}
+					}
+				}
+
+				/*
+				printf("\n---\n");
+				printf("Username: %s\n",username);
+				printf("password: %s\n", password);
+				printf("UID: %s\n", UID);
+				printf("GID: %s\n", GID);
+				printf("GECOS: %s\n", GECOS);
+				printf("directory: %s\n", directory);
+				printf("shell: %s\n", shell);
+				printf("\n---\n");
+				*/
 
 				// CHECK STUFF
 
@@ -91,50 +150,51 @@ int main(int argc, char *argv[])
 					faulthy_row = true;
 					free(username);
 				}
-				else if (strlen(username) == 0)
+				else if(countingSemiColons != 6)
 				{
-					printf("Line %d: Invalid format.\n", lineNumber);
+					printf("Line %d: Found %d semicolons, 6 is required. Invalid format.\n", lineNumber,countingSemiColons);
 					faulthy_row = true;
 					free(username);
 				}
-				else if (strlen(password) == 0)
+				else if (strlen(username) == 0 || strlen(username) > 32)
 				{
-					printf("Line %d: Invalid format.\n", lineNumber);
+					printf("Line %d: Username is invalid. Must be 1-32 charachers.\n", lineNumber);
 					faulthy_row = true;
 					free(username);
 				}
 				else if (strlen(UID) == 0)
 				{
-					printf("Line %d: Invalid format.\n", lineNumber);
+					printf("Line %d: Missing UID, invalid format.\n", lineNumber);
 					faulthy_row = true;
 					free(username);
 				}
 				else if (strlen(GID) == 0)
 				{
-					printf("Line %d: Invalid format.\n", lineNumber);
-					faulthy_row = true;
-					free(username);
-				}
-				else if (strlen(GECOS) == 0)
-				{
-					printf("Line %d: Invalid format.\n", lineNumber);
+					printf("Line %d: Missing GID, invalid format.\n", lineNumber);
 					faulthy_row = true;
 					free(username);
 				}
 				else if (strlen(directory) == 0)
 				{
-					printf("Line %d: Invalid format.\n", lineNumber);
+					printf("Line %d: Missing directory, invalid format.\n", lineNumber);
 					faulthy_row = true;
 					free(username);
 				}
 				else if (strlen(shell) == 0)
 				{
-					printf("Line %d: Invalid format.\n", lineNumber);
+					printf("Line %d: Missing shell, invalid format.\n", lineNumber);
 					faulthy_row = true;
 					free(username);
-				}else if(GID[0] == '-')
+				}
+				else if(GID[0] == '-')
 				{
 					printf("Line %d: GID has to be a positive number. Got \"%s\" \n", lineNumber,GID);
+					faulthy_row = true;
+					free(username);
+				}
+				else if (UID[0] == '-')
+				{
+					printf("Line %d: UID has to be a positive number. Got \"%s\" \n", lineNumber, UID);
 					faulthy_row = true;
 					free(username);
 				}
@@ -159,10 +219,6 @@ int main(int argc, char *argv[])
 						free(newValue);
 					}
 
-				}
-
-				if(faulthy_row){
-					printf("%s\n",line);
 				}
 
 				free(UID);
@@ -202,3 +258,4 @@ int main(int argc, char *argv[])
 		return EXIT_SUCCESS;
 	}
 }
+
